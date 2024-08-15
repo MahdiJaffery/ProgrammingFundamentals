80a44d9fb3461b1587528d2310b54e384302cec4
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-08-16 00:57:05 +0500

    Vowel or Consonant

---
+++ Python/Manual01.py/Question05.py
@@ -0,0 +1,11 @@
+value = input("Enter an alphabet: ")
+
+if not ((value >= "A" and value <= "Z") or (value >= "a" and value <= "z")):
+    print("Invalid Input")
+else:
+    value = value.lower()
+
+    if value == "a" or value == "e" or value == "i" or value == "o" or value == "u":
+        print("Alphabet is a Vowel")
+    else:
+        print("Alphabet is a Consonant")
