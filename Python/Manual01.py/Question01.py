d70085fc6cb94663ab5ddd702f8046d37e760d1b
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-08-16 00:55:00 +0500

    Comparison of Variables

---
+++ Python/Manual01.py/Question01.py
@@ -0,0 +1,41 @@
+print("Enter the Value of a: ")
+a = input()
+print("Enter the Value of b: ")
+b = input()
+print("Enter the Value of c: ")
+c = input()
+print("Enter the Value of d: ")
+d = input()
+print("Enter the Value of e: ")
+e = input()
+
+if a > b and a > c and a > d and a > e:
+    print(a + " is the greatest number")
+
+if b > a and b > c and b > d and b > e:
+    print(b + " is the greatest number")
+
+if c > a and c > b and c > d and c > e:
+    print(c + " is the greatest number")
+
+if d > a and d > b and d > c and d > e:
+    print(d + " is the greatest number")
+
+if e > a and e > b and e > c and e > d:
+    print(e + " is the greatest number")
+
+
+if a < b and a < c and a < d and a < e:
+    print(a + " is the smallest number")
+
+if b < a and b < c and b < d and b < e:
+    print(b + " is the smallest number")
+
+if c < a and c < b and c < d and c < e:
+    print(c + " is the smallest number")
+
+if d < a and d < b and d < c and d < e:
+    print(d + " is the smallest number")
+
+if e < a and e < b and e < c and e < d:
+    print(e + " is the smallest number")
