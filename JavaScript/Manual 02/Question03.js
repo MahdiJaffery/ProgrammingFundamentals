334b3d75c22e980a24a7bc635ab5174d40129d88
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-09-06 00:26:44 +0500

    Basic Calculator

---
+++ JavaScript/Manual 02/Question03.js
@@ -0,0 +1,34 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+rl.question("Enter your percentage: ", function (percentage) {
+    switch (true) {
+        case (percentage >= 80):
+            console.log("A+");
+            break;
+        case (percentage >= 70):
+            console.log("A");
+            break;
+        case (percentage >= 60):
+            console.log("B");
+            break;
+        case (percentage >= 50):
+            console.log("C");
+            break;
+        case (percentage >= 40):
+            console.log("D");
+            break;
+        case (percentage < 40):
+            console.log("F");
+            break;
+        default:
+            console.log("Invalid Input");
+            break;
+    }
+
+    rl.close();
+})
